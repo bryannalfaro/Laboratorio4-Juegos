b3aae957132296defd00d8e596f622746b60baf9
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-04-20 17:39:22 -0600

    requerimientos completos

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gun : MonoBehaviour
+
+{
+    public float damage = 50;
+    public float range = 100;
+    public Camera camara;
+    public AudioClip gun_sound;
+
+    private GameObject scoreObject;
+    private Text score;
+
+    private int scoreCantidad = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreObject = GameObject.FindGameObjectWithTag("Score");
+        score = scoreObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+
+            Shoot();
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.PlayOneShot(gun_sound);
+        }
+
+        if (score) {
+            score.text = "Score: " + scoreCantidad.ToString();
+        }
+    }
+
+   void Shoot ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(camara.transform.position, camara.transform.forward, out hit, range)) {
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+            if (target.CompareTag("Player"))
+            {
+                scoreCantidad = scoreCantidad + 1;
+            }
+        
+        };
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Target : MonoBehaviour
+{
+    private Vector3 posicionInicial;
+    private int velocidad_Programa = 2;
+    public float health = 50;
+
+   
+
+    void Start()
+    {
+        posicionInicial = transform.localPosition;
+
+    }
+
+    void Update()
+    {
+        transform.localPosition = posicionInicial + new Vector3(0, Mathf.Sin(Time.time * velocidad_Programa), 0);
+
+    }
+
+    public void TakeDamage(float amount) {
+
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    
+    }
+
+    void Die()
+    {
+         
+        Destroy(gameObject);
+       
+    }
+
+}
